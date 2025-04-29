6ee800935ef5d655412a97a694b6b9231cab5d75
xcawolfe@ <xcawolfe@amazon.com>
2025-04-28 22:28:18 -0700

    al2023: update disk and appliance

--- build-tests/x86/al2023/test-image-live-disk/disk.sh
+++ build-tests/x86/al2023/test-image-live-disk/disk.sh
@@ -7,5 +7,5 @@ ROOT_UUID=$(blkid --list-one --output value --match-token LABEL="/" -s UUID)
 dracut \
   --uefi \
   --kver $(uname -r) \
-  --kernel-cmdline "root=UUID=${ROOT_UUID} rootwait ro security=selinux nvme_core.io_timeout=4294967295 rd.shell=0 selinux=0 security=selinux console=ttyS0,115200n8 earlyprintk=ttyS0,115200 systemd.log_level=debug systemd.log_target=console rd.shell=1" \
---force /boot/efi/EFI/BOOT/BOOTX64.EFI
+  --force /boot/efi/EFI/BOOT/BOOTX64.EFI \
+  --kernel-cmdline "root=UUID=${ROOT_UUID} nvme_core.io_timeout=4294967295  console=ttyS0,115200n8  systemd.log_level=debug systemd.log_target=console rd.shell=1" 
