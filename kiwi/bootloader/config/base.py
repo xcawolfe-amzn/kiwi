a2041b2170aa25b6d356060d7b058cde55c0357d
xcawolfe@ <xcawolfe@amazon.com>
2025-05-12 14:42:52 -0700

    config/base: add overlay root filesystem check step

--- kiwi/bootloader/config/base.py
+++ kiwi/bootloader/config/base.py
@@ -595,6 +595,7 @@ class BootLoaderConfigBase(ABC):
         if self.system_is_mounted:
             # Rebuild security context
             setup = SystemSetup(self.xml_state, self.root_mount.mountpoint)
+            from pudb import set_trace; set_trace()
             if not self.root_filesystem_is_overlay:
                 setup.setup_selinux_file_contexts()
             # Umount system
