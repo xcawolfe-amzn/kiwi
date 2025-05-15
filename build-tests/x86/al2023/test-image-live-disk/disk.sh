af49dee28db07fdfdafbd3fbbc8e6ab101706062
xcawolfe@ <xcawolfe@amazon.com>
2025-05-15 16:35:57 -0700

    al2023: add disk.sh and add the appliance

--- build-tests/x86/al2023/test-image-live-disk/disk.sh
+++ build-tests/x86/al2023/test-image-live-disk/disk.sh
@@ -1,3 +1,77 @@
 #!/bin/bash
-echo /etc/fstab
+set -x
+# Get Root partition UUID
+
+# Uses what is in /dev, ensure the label ROOT is not used by another mounted volume
+echo "list blk"
+blkid
+
+
+echo "See fstab original"
+cat /etc/fstab
+ROOT_VERITY="/dev/mapper/root"
+
+# Get the root device from kiwi created, should be a loop dev
+ROOT_DEV=$(blkid --list-one --output device --match-token LABEL="ROOT") 
+
+# Get the specific loop device 
+LOOP_DEV=$(echo "${ROOT_DEV}" | sed 's/p[0-9]\+$//')
+HASH_DEV="/tmp/root-verity.img"
+
+VERITY_FILE="./verity-info.txt"
+
+# Remap verity with the symlink for verity device
+sed -i "1s|UUID=[0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}|${ROOT_VERITY}|" /etc/fstab
+
+
+echo "list blk"
+blkid
+
+
+echo "See fstab changes"
+cat /etc/fstab
+
+VERITY_DEV=$(blkid --list-one --output device --match-token LABEL="VERITY") 
+
+# Unmount the verity parition
+umount /verity
+
+# Create a verity partition in the loop device 
+#NOTE: not using the Kiwi parition b/c of mountpoint 
+# sgdisk -n 2:0:+1G -t 2:8300 -c 2:"VERITY" $LOOP_DEV
+# Generate verity hash
+
+tee /etc/udev/rules.d/99.partlabel.rules > /dev/null << 'EOF'
+ENV{ID_PART_ENTRY_NAME}==\"ROOT\", SYMLINK+=\"disk/by-partlabel/ROOT\"
+ENV{ID_PART_ENTRY_NAME}==\"VERITY\", SYMLINK+=\"disk/by-partlabel/VERITY\"
+EOF
+
+veritysetup format "${ROOT_DEV}" "${HASH_DEV}" \
+  --hash-offset=0 --data-block-size=4096 --hash-block-size=4096 \
+  | tee $VERITY_FILE
+
+# Create verity data in partition
+dd if="${VERITY_FILE}" of="${VERITY_DEV}" bs=4096 conv=fsync
+
+# set ROOT_HASH
+ROOT_HASH=$(grep "Root hash:" "${VERITY_FILE}" | awk '{print $3}')
+echo $ROOT_HASH
+
+# Verify verity setup and create /dev/mapper/verity_root
+veritysetup open "${ROOT_DEV}" verity_root "${VERITY_DEV}" "${ROOT_HASH}" \
+  --hash-offset=0
+if [ $? -ne 0 ]; then
+    echo "Error: Verity setup failed"
+    exit 1
+fi
+
+  # --kernel-cmdline "root=/dev/mapper/root rootwait roothash=${ROOT_HASH} nvme_core.io_timeout=500 console=ttyS0,115200n8 systemd.log_level=debug systemd.log_target=console rd.shell=1 systemd.verity_root_data=/dev/disk/by-label/ROOT systemd.verity_root_hash=/dev/disk/by-partlabel/VERITY systemd.verity_root_options=ignore-zero-blocks,ignore-corruption" 
+dracut \
+  --uefi \
+  --kver $(uname -r) \
+  --force /boot/efi/EFI/BOOT/BOOTX64.EFI \
+  --add-drivers "dm-verity erofs overlay vfat nls_cp437 nls_iso8859-1" \
+  --force-drivers "dm-verity erofs overlay vfat nls_cp437 nls_iso8859-1" \
+  --add "dm crypt systemd systemd-veritysetup systemd-initrd" \
+  --kernel-cmdline "root=/dev/mapper/root rootwait roothash=${ROOT_HASH} nvme_core.io_timeout=500 console=ttyS0,115200n8 systemd.log_level=debug systemd.log_target=console rd.shell=1 systemd.verity_root_options=ignore-zero-blocks,ignore-corruption" 
 
