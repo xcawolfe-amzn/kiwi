9ebaf22e6193f545aeb60950c65da5a00afa37e9
xcawolfe@ <xcawolfe@amazon.com>
2025-05-16 08:49:56 -0700

    systemd_boot: test removing wipe step

--- kiwi/bootloader/config/systemd_boot.py
+++ kiwi/bootloader/config/systemd_boot.py
@@ -178,7 +178,7 @@ class BootLoaderSystemdBoot(BootLoaderSpecBase):
                 '--entry-token', 'os-id'
             ]
         )
-        Path.wipe(f'{root_dir}/boot/loader')
+        # Path.wipe(f'{root_dir}/boot/loader')
         self._write_kernel_cmdline_file(root_dir)
 
         # copy kernel and initrd
