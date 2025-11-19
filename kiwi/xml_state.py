ee02d1362868d39147c77a8d09cfdfbae77653e3
xcawolfe@ <xcawolfe@amazon.com>
2025-11-19 13:16:31 -0800

    fix to test for ec2_layout

--- kiwi/xml_state.py
+++ kiwi/xml_state.py
@@ -1621,7 +1621,7 @@ class XMLState:
         """
         try:
             ec2_layout_value = self.build_type.get_ec2_layout()
-            return ec2_layout_value == "true" if ec2_layout_value is not None else False
+            return ec2_layout_value == "true" if ec2_layout_value else False
         except AttributeError:
             # Attribute doesn't exist in older XML files
             return False
